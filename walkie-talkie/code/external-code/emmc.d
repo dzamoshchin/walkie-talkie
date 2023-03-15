dcb8aa25fa01d0b8f6bd6f1ca56a0322890eac81
Ari Webb <arijwebb@gmail.com>
2023-03-15 14:30:46 -0700

    added all fat32 code

---
+++ walkie-talkie/code/external-code/emmc.d
@@ -0,0 +1,7 @@
+external-code/emmc.o: external-code/emmc.c \
+ /home/arijwebb/classrepos/cs140e/libpi/include/rpi.h \
+ /home/arijwebb/classrepos/cs140e/libpi/libc/demand.h \
+ /home/arijwebb/classrepos/cs140e/libpi/include/gpio.h \
+ /home/arijwebb/classrepos/cs140e/libpi/include/rpi-constants.h \
+ /home/arijwebb/classrepos/cs140e/libpi/include/your-prototypes.h \
+ external-code/emmc.h external-code/mbox.h
