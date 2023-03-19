ac66097dcb77367d3d03aee25823cbda8b4c8a6a
dzamoshchin <danzamoshchin@gmail.com>
2023-03-19 16:17:56 -0700

    organize

--- walkie-talkie/code/external-code/unicode-utf8.d
+++
@@ -1,7 +0,0 @@
-external-code/unicode-utf8.o: external-code/unicode-utf8.c \
- /home/arijwebb/classrepos/cs140e/libpi/include/rpi.h \
- /home/arijwebb/classrepos/cs140e/libpi/libc/demand.h \
- /home/arijwebb/classrepos/cs140e/libpi/include/gpio.h \
- /home/arijwebb/classrepos/cs140e/libpi/include/rpi-constants.h \
- /home/arijwebb/classrepos/cs140e/libpi/include/your-prototypes.h \
- external-code/unicode-utf8.h
