b02f0cb0f7149797ea00d379c979746a214146ab
Arnav Joshi <arnavjoshi@DN525feh.SUNet>
2023-03-20 20:02:05 -0700

    revert i2s

--- walkie-talkie/code/i2s/i2s.c
+++ walkie-talkie/code/i2s/i2s.c
@@ -8,9 +8,8 @@ void i2s_init(void) {
 
     //Step 1: set the BCLK, DIN and LRCL lines to ALT0
     gpio_set_function(I2S_PIN_CLK, GPIO_FUNC_ALT0);
-//    gpio_set_function(I2S_PIN_DIN, GPIO_FUNC_ALT0);
+    gpio_set_function(I2S_PIN_DIN, GPIO_FUNC_ALT0);
     gpio_set_function(I2S_PIN_FS, GPIO_FUNC_ALT0);
-    gpio_set_function(I2S_PIN_DOUT, GPIO_FUNC_ALT0);
 
     dev_barrier();
 
@@ -26,51 +25,14 @@ void i2s_init(void) {
     
     // Channel 1 enabled with width 32 and offset 0
     PUT32(I2S_RXC, (1 << I2S_RXC_CH1WEX) | (8 << I2S_RXC_CH1WID_LB) | (1 << I2S_RXC_CH1EN));
-    PUT32(I2S_TXC, (1 << I2S_TXC_CH1WEX) | (8 << I2S_TXC_CH1WID_LB) | (1 << I2S_TXC_CH1EN));
     
-    // clear TX and RX and disable STBY
-    PUT32(I2S_CS, (1 << I2S_CS_STBY) | (1 << I2S_CS_RXCLR) | (1 << I2S_CS_TXCLR));
-
-//    PUT32(I2S_CS, GET32(I2S_CS) | (0b01 << 5));  // TXTHR to less than full
+    // Set up RX and disable STBY
+    PUT32(I2S_CS, (1 << I2S_CS_STBY) | (1 << I2S_CS_RXCLR) | (1 << I2S_CS_RXON));
 
     // Enable I2S
     PUT32(I2S_CS, GET32(I2S_CS) | (1 << I2S_CS_EN));
 
     dev_barrier();
-}
-
-void is2_clear() {
-    PUT32(I2S_CS, GET32(I2S_CS) & ~(1 << I2S_CS_EN));
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) | (1 << I2S_CS_TXCLR) | (1 << I2S_CS_RXCLR));
-}
-
-void is2_enable() {
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) | (1 << I2S_CS_EN));
-    dev_barrier();
-}
-
-void i2s_disable() {
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) & ~(1 << I2S_CS_EN));
-}
-
-void i2s_enable_rx() {
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) & ~(1 << I2S_CS_TXON));
-    PUT32(I2S_CS, GET32(I2S_CS) | (1 << I2S_CS_RXON));
-}
-
-void i2s_enable_tx() {
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) & ~(1 << I2S_CS_RXON));
-    PUT32(I2S_CS, GET32(I2S_CS) | (1 << I2S_CS_TXON));
-}
-
-void i2s_disable_tx() {
-    dev_barrier();
-    PUT32(I2S_CS, GET32(I2S_CS) & ~(1 << I2S_CS_TXON));
 }
 
 int32_t i2s_read_sample(void) {
@@ -79,25 +41,4 @@ int32_t i2s_read_sample(void) {
     while ((GET32(I2S_CS) & (1 << I2S_CS_RXD)) == 0) {};
     // then return sample of FIFO
     return GET32(I2S_FIFO);
-}
-
-void i2s_transmit() {
-    i2s_enable_tx();
-    while ((GET32(I2S_CS) & (1 << I2S_CS_TXW)) == 0) {
-        printk("Transmitting tx fifo....");
-    }
-    printk("\nTransmit complete, disabling TX...\n");
-    i2s_disable_tx();
-    dev_barrier();
-}
-
-void i2s_write_sample(int32_t val) {
-    dev_barrier();
-
-    if ((GET32(I2S_CS) & (1 << I2S_CS_TXD)) == 0) {
-        printk("FIFO is full, queuing transmit...\n");
-        i2s_transmit();
-    }
-    // then return sample of FIFO
-    return PUT32(I2S_FIFO, val);
 }
--- walkie-talkie/code/tests/2-i2s-dump.c
+++ walkie-talkie/code/tests/2-i2s-dump.c
@@ -15,7 +15,7 @@ void notmain(void) {
     int32_t *buf = (int32_t *)kmalloc(N * sizeof(int32_t));
 
     i2s_init();
-    i2s_enable_rx();
+    // i2s_enable_rx();
 
     unsigned start = timer_get_usec();
     for (int i = 0; i < N; i++) {
