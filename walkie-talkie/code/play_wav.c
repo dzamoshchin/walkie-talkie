8b4b9cd737f81eebf31a7342b002dbdb93641134
dzamoshchin <danzamoshchin@gmail.com>
2023-03-22 23:32:18 -0700

    play any wav

--- walkie-talkie/code/play_wav.c
+++ walkie-talkie/code/play_wav.c
@@ -1,52 +1,54 @@
 #include "rpi.h"
 #include "pwm.h"
+#include "fat32.h"
+#include "WAV.h"
 
-// #include "samples/sin8.h"
-// #include "samples/sound2.h"
-#include "samples/sound3.h"
+#define SAMPLE_RATE 44100
 
-struct wav_format {
-   uint32_t description; // should be 'RIFF'
-   uint32_t filesize; // little endian
-   uint32_t wav_header; // should be 'WAVE'
-   uint32_t wav_format; // should be 'fmt '
-   uint32_t type_format_size; // should be 16, as in the above four fields are 16 bytes 
-   uint16_t wav_type; // 0x01 is PCM
-   uint16_t num_channels; // 0x01 is mono, 0x02 is stereo, etc.
-   uint32_t sample_rate; // e.g., 44100, little-endian
-   uint32_t rate_plus; // sample rate * bits per sample * channels / 8
-   uint16_t bits_per_sample; // e.g., 16
-   uint32_t data_mark; // should be 'data'
-   uint32_t data_size; // little endian
-   uint8_t *data; // may be 16-bit
-};
+void config_fs(fat32_fs_t *fs, pi_dirent_t *root) {
+    printk("Reading the MBR.\n");
+    mbr_t *mbr = mbr_read();
 
+    printk("Loading the first partition.\n");
+    mbr_partition_ent_t partition;
+    memcpy(&partition, mbr->part_tab1, sizeof(mbr_partition_ent_t));
+    assert(mbr_part_is_fat32(partition.part_type));
+
+    printk("Loading the FAT.\n");
+    *fs = fat32_mk(&partition);
+
+    printk("Loading the root directory.\n");
+    *root = fat32_get_root(fs);
+}
 
 void notmain ()
 {
-    pwm_init();
-    audio_init(sample_freq);
+    kmalloc_init();
+    pi_sd_init();
 
-    while (1) {
-        printk("starting play\n");
-        if (bits_per_sample == 8) {
-            while (1) {
-                for (unsigned int sample = 0; sample < sizeof(wav_data); sample++) {
-                    unsigned status = pwm_get_status();
-                    while (status & PWM_FULL1) {
-                        status = pwm_get_status();
-                    }
-                    uint8_t pcm = wav_data[sample];
-                    // mono
-                    pwm_write( pcm ); // output to channel 0
-                    pwm_write( pcm ); // output to channel 1
-                }
-                if (!repeat) break;
-            }
-        } else {
-            printk("16 bit not implemented\n");
+    fat32_fs_t fs;
+    pi_dirent_t root;
+    config_fs(&fs, &root);
+
+    pwm_init();
+    audio_init(SAMPLE_RATE);
+
+    pi_file_t *file = fat32_read(&fs, &root, "RICK.WAV");
+
+    file->data = file->data + sizeof(wav_header_t);
+
+    int16_t *data = (int16_t*)file->data;
+
+    printk("starting play\n");
+    for (unsigned int sample = 0; sample < (file->n_data - sizeof(wav_header_t)) / sizeof(int16_t); sample++) {
+        unsigned status = pwm_get_status();
+        while (status & PWM_FULL1) {
+            status = pwm_get_status();
         }
-        printk("done playing\n");
-        delay_ms(1000);
+        unsigned wave = data[sample] + 0x8000;
+        uint8_t pcm = wave>>8;
+        pwm_write( pcm );
+        pwm_write( pcm );
     }
+    printk("done playing\n");
 }
